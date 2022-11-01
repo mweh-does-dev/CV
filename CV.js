f3ead29bbc16b7c9b121a8447ee18eeea0230b63
mweh-does-dev <josefin.berntsson@chasacademy.se>
2022-11-01 19:46:50 +0100

    Changed js code to DRY code

--- CV.js
+++ CV.js
@@ -4,46 +4,72 @@ fetch("./index.json")
     // koppla ihop json med js
 
 
+
     const workexperience = document.querySelector("#workexperience");
-    for (let experience of obj.workexperience){
-        console.log(experience);
-
-        const titleElement = document.createElement("li");
-        titleElement.innerHTML = "<strong>" + experience.title + "</strong>" + "&nbsp; " + experience.year;
-        workexperience.append(titleElement);
-//loop för workexxperience
-
-       for (let description of experience.description){
-        const descriptionElement = document.createElement("li");
-        descriptionElement.innerHTML = description;
-        workexperience.append(descriptionElement);
-        console.log(description);
-        console.log(experience.description);
-
-       }
-//loop för description
-    }
-
-    
     const education = document.querySelector("#education");
 
-    for (let educationCv of obj.education){
-        console.log(education);
+    function renderCv(jsonData, ulElement){ 
+        for (let experience of jsonData){
+            const titleElement = document.createElement("li");
+            titleElement.innerHTML = "<strong>" + experience.title + "</strong>" + "&nbsp; " + experience.year;
+            ulElement.append(titleElement);
+            
 
-        const titleElement = document.createElement("li");
-        titleElement.innerHTML = "<strong>" + educationCv.title + "</strong>" + "&nbsp; " + educationCv.year;
-        education.append(titleElement);
-//loop för education
-       for (let description of educationCv.description){
+            for (let description of experience.description){
+                const descriptionElement = document.createElement("li");
+                descriptionElement.innerHTML = description;
+                ulElement.append(descriptionElement);
+             
+            }
         
-         const descriptionElement = document.createElement("li");
-         descriptionElement.innerHTML = description;
-            education.append(descriptionElement);
-
-       }
-//loop för description
+        }
     }
+    renderCv(obj.workexperience, workexperience);
+    renderCv(obj.education, education);
+
+    
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+   
+
+      
+
+       
+ 
+
+
+
+    
+   // const education = document.querySelector("#education");
+
+    //for (let educationCv of obj.education){
+
+       // const titleElement = document.createElement("li");
+       // titleElement.innerHTML = "<strong>" + educationCv.title + "</strong>" + "&nbsp; " + educationCv.year;
+      //  education.append(titleElement);
+//loop för education
+      // for (let description of educationCv.description){
+        
+         //const descriptionElement = document.createElement("li");
+         //descriptionElement.innerHTML = description;
+            //education.append(descriptionElement);
+
+     //  }
+//loop för description
+   // }
  
     
 
-});
+//})
